d383764c077f8a00f053c5d4424574b2eb5f3db6
Tejas <Tejashiray2123@gmail.com>
2024-05-08 17:10:05 +0530

    Map,list Interface

---
+++ Day12/Mapdemo.java
@@ -0,0 +1,15 @@
+package Day12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapdemo{
+    public Map<String,Integer>getMapString() {
+        Map<String, Integer> mapString = new HashMap<>();
+        mapString.put("A",14);
+        mapString.put("B",27);
+        mapString.put("C",45);
+
+        return mapString;
+    }
+}
